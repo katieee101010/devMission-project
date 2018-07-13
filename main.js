1dd11c2b749eed32b171827bfe78a729b07c832e
katieee101010 <kstardog@gmail.com>
2018-07-12 18:59:47 -0700

    Airtable

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //this url is from airtable the Authentification section
-var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=YOUR_API_KEY'
+var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyeqGWuvGequynvo';
 
 //this is where we get the JSON data from Airtable
 $.getJSON( airtable_list_url, function( data ) {
