747db469781efff6de2d97dc898cde373e6646ba
katieee101010 <kstardog@gmail.com>
2018-07-19 17:45:16 -0700

    1

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 //this url is from airtable the Authentification section
-var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyeqGWuvGequynvo';
-
+var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyppYGfrsTMTEfnY';
 //this is where we get the JSON data from Airtable
-$.getJSON( airtable_list_url, function( data ) {
+  $.getJSON( airtable_list_url, function( data ) {
     var items = [];
     $.each( data.records, function( key, val ) {
       console.log(val.fields)
-      items.push(`<h2>${val.fields['Name']}</h2>`);
+      items.push(`<h2>${val.fields['Name','district']}</h2>`);
     });
     $(".list-view").append(items.join(''));
   });
