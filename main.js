dff1107f8cb11665dfc1a5e1a9764e9942708dab
katieee101010 <kstardog@gmail.com>
2018-08-02 13:16:55 -0700

    card and detail view?

--- main.js
+++ main.js
@@ -2,22 +2,64 @@
 var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyppYGfrsTMTEfnY';
 //this is where we get the JSON data from Airtable
 $.getJSON( airtable_list_url, function( data ) {
-    var items = [];
-    $.each( data.records, function( key, val ) {
-      console.log(val.fields)
-      items.push(`<h2>${val.fields['type of food']}</h2>`);
+    var html = [];
+    html.push(`<div class="row">`);
+    $.each( data.records, function(index, val){
+      var id = val.id;
+      var fields = val.fields;
+      var TypeOfFood = fields["type of food"];
+      var pictureUrl = fields["picture"] ? fields["Pictures"][0].url : '';
+      var name = fields["name"];
+      var district = fields["district"];
+      var itemHTML = listView(id, TypeOfFood, pictureUrl);
+      html.push(itemHTML);
     });
+    html.push(`</div>`);
     $(".list-view").append(items.join(''));
   });
-var listView = function(id, name, picture){
-  return `<div class="col-sm-3">
-<div class="card" style="width: 18rem;">
-  <a href="?id=${id}"><img class="card-img-top" src="${picture}"></a>
-  <div class="card-body">
-   <h2><a href="?id=${id}">${typeoffood}</a></h2>
-   <div class="d-flex justify-content-between align-items-center">
-  </div>
-</div>
-</div>
-</div>`;
-}
+
+  var detailView = function(id, TypeOfFood, pictureUrl){
+    return `<div class="col-sm-12">
+    <div class="card mb-4 box-shadow">
+    <img class="card-img-top" src="${pictureUrl}">
+    <div class="card-body">
+    <h2>${TypeOfFood}</h2>
+    <p class="card-text">${name}</p>
+        <p class="card-text">${district}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <small class="text-muted">${address}</small>
+        </div>
+        ${website ? `<a href="${website}">${website}</a>`: ``}
+        <hr />
+      </div>
+    </div>
+  </div>`;
+  }
+
+  var getDataForId = function(id) {
+    $.getJSON( airtable_list_url, function( record ) {
+      var html = [];
+      html.push(`<div class="row">`);
+        var id = record.id;
+        var fields = record.fields;
+        var TypeOfFood = fields["type of food"];
+        var name = fields["name"];
+        var pictureUrl = fields["picture"] ? fields["Pictures"][0].url : '';
+        var district = fields["district"];
+        var address = fields["address"];
+        var link = fields["link to yelp"];
+  
+        var itemHTML = detailView(id, TypeOfFood, name, pictureUrl, district, address, link);
+        html.push(itemHTML);
+      html.push(`</div>`);
+      $(".detail-view").append(html.join(""));
+    });
+  }
+
+  var id - getParameterByName("id");
+
+  if (id) {
+    getDataForId(id);
+  } else {
+    getDataForList();
+  }
