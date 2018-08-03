84b6588a1577cce52618abb704a851d2086919f9
katieee101010 <kstardog@gmail.com>
2018-08-02 17:15:46 -0700

    getParameterByName

--- main.js
+++ main.js
@@ -68,6 +68,14 @@ $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyp
       $(".detail-view").append(html.join(""));
     });
   }
+  var getParameterByName = function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
   var id = getParameterByName("id");
 
