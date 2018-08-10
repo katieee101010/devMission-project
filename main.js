7c1291a5ffd750b3d772434518f9544fd79d7122
katieee101010 <kstardog@gmail.com>
2018-08-09 18:38:41 -0700

    detail view working

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // this url is from airtable the Authentification section
 // var airtable_list_url='https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyppYGfrsTMTEfnY';
 // this is where we get the JSON data from Airtable
+var getParameterByName = function(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var api_key = 'keyppYGfrsTMTEfnY';
 
 var listView = function(id, TypeOfFood, coverPic){
   return `<div class="col-sm-3">
@@ -15,58 +25,6 @@ var listView = function(id, TypeOfFood, coverPic){
 </div>`;
 }
 
-if (TypeOfFood == "Greek"){
-  coverPic = <a href="http://www.toloukoumi.com/images/easyblog_articles/19/to-loukoumi-traditional-greek-restaurant-astoria-greek-food-cuisine.jpg"></a>
-}
-
-else if( TypeOfFood == "Japanese" ){
-  coverPic = <a href="https://cdn.foodadvisor.com.sg/uploads/images/image_default_95a69dae181af6.jpg"></a>
-}
-
-else if( TypeOfFood == "Mexican" ){
-  coverPic = <a href="https://primosmex.com/wp-content/uploads/2016/09/PMX-Food-Collage-1024x768.jpg"></a>
-}
-
-else if( TypeOfFood == "Chinese" ){
-  coverPic = <a href="https://alacartedelivery.com/wp-content/uploads/2017/06/taekaway-chinese-food-header-dark.jpg"></a>
-}
-
-else if( TypeOfFood == "Korean" ){
-  coverPic = <a href="http://woorinara.sg/wp-content/uploads/2016/11/Woorinara-Korean-Restaurant-Bibimbap.jpg"></a>
-}
-
-else if( TypeOfFood == "Indian" ){
-  coverPic = <a href="http://bigapplecurry.files.wordpress.com/2012/11/istock_000019639558_medium.jpg"></a>
-}
-
-else if( TypeOfFood == "American" ){
-  coverPic = <a href="https://www.eatatfrontstreet.net/ressources/images/182106c0fef0.jpg"></a>
-}
-
-else if( TypeOfFood == "Italian" ){
-  coverPic = <a href="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/8/7/1/FN_Italian-Favorites-Opener_s4x3.jpg.rend.hgtvcom.966.725.suffix/1438990067345.jpeg"></a>
-}
-
-else if( TypeOfFood == "Thai" ){
-  coverPic = <a href="https://assets.bonappetit.com/photos/57afb7eaf1c801a1038bd380/16:9/w_1000,c_limit/pad-thai-940.jpg"></a>
-}
-
-else if( TypeOfFood == "Pizza"){
-  coverPic = <a href="https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_574,q_80,w_945/tbagzeanc4qhrnlanzgi.jpg"></a>
-}
-
-else if( TypeOfFood == "Vietnamese" ){
-  coverPic = <a href="https://globetrottr.org/wp-content/uploads/2016/06/vietnamfood_1377074024-500x330.jpg"></a>
-}
-
-else if( TypeOfFood == "Breakfast/ Brunch" ){
-  coverPic = <a href="http://hedonistshedonist.com/wp-content/uploads/2017/11/southern-brunch-product.jpg"></a>
-}
-
-else if( TypeOfFood == "Ice Cream" ){
-  coverPic = <a href="https://lifehacker.rs/wp-content/uploads/2017/02/sladoled-1080x601.jpgS"></a>
-}
-
 var getDataForList = function(){
 $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyppYGfrsTMTEfnY`, function( data ) {
     var html = [];
@@ -74,7 +32,7 @@ $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyp
     $.each( data.records, function(index, val){
       var id = val.id;
       var fields = val.fields;
-      var TypeOfFood = fields["type of food"];
+      var TypeOfFood = fields["foods"];
       var pictureUrl = fields["picture"] ? fields["picture"][0].url : '';
       var itemHTML = listView(id, TypeOfFood, pictureUrl);
       html.push(itemHTML);
@@ -84,17 +42,18 @@ $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyp
   });
 }
 
-  var detailView = function(id, TypeOfFood, pictureUrl){
+  var detailView = function(id, TypeOfFood, name, pictureUrl, district, address, link){
     return `<div class="col-sm-12">
    <div class="card mb-4 box-shadow">
+   <h2>${TypeOfFood}</h2>
    <img class="card-img-top" src="${pictureUrl}">
    <div class="card-body">
-   <h2>${TypeOfFood}</h2>
-   <p class="card-text">${name}</p>
+      <h2>${name}</h2>
        <p class="card-text">${district}</p>
+       <h5>${address}</h5>
        <div class="d-flex justify-content-between align-items-center">
-         <small class="text-muted">${address}</small>
        </div>
+       <p class="card-text">Link to yelp for more information about ${name}: </p>
         ${link ? `<a href="${link}">${link}</a>`: ``}
        <hr />
       </div>
@@ -103,31 +62,23 @@ $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyp
   }
 
   var getDataForId = function(id) {
-    $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201?api_key=keyppYGfrsTMTEfnY`, function( record ) {
+    $.getJSON( `https://api.airtable.com/v0/appYyE1ErirB57dsj/Table%201/${id}?api_key=keyppYGfrsTMTEfnY`, function( record ) {
       var html = [];
       html.push(`<div class="row">`);
         var id = record.id;
         var fields = record.fields;
-        var TypeOfFood = fields["type of food"];
+        var TypeOfFood = fields["foods"];
         var name = fields["name"];
         var pictureUrl = fields["picture"] ? fields["picture"][0].url : '';
         var district = fields["district"];
         var address = fields["address"];
-        var link = fields["link to yelp"];
+        var link = fields["yelp"];
   
         var itemHTML = detailView(id, TypeOfFood, name, pictureUrl, district, address, link);
         html.push(itemHTML);
       html.push(`</div>`);
       $(".detail-view").append(html.join(""));
     });
-  }
-  var getParameterByName = function(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 
   var id = getParameterByName("id");
